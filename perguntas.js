16e000ae401387d3817c66ab4edbea32daf719f9
RafaelaGauna07 <00001074143759sp@al.educacao.sp.gov.br>
2024-11-26 15:39:29 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro imperador romano?',
+    'Augusto'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Independência do Brasil?',
+    '1822'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o líder da Revolução Francesa?',
+    'Napoleão Bonaparte.'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a causa principal da Primeira Guerra Mundial?',
+    ' O assassinato do arquiduque Francisco Ferdinando.'
+)
